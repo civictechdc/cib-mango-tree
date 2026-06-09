1e49a7b8e305cf5f2592fc2866d84770d7ea5ef2
Kristijan Armeni <kristijan.armeni@gmail.com>
2026-06-09 10:13:20 -0400

    move _remove_motw to gui/utils.py

--- src/cibmangotree/gui/utils.py
+++ src/cibmangotree/gui/utils.py
@@ -34,3 +34,19 @@ def open_directory_explorer(path: str):
             raise OSError(f"Unsupported POSIX platform: {sys.platform}")
     else:
         raise OSError(f"Unsupported operating system: {os.name}")
+
+
+def _remove_motw():
+
+    FILES = [
+        "pythonnet/runtime/Python.Runtime.dll",
+        "webview/lib/Microsoft.Web.WebView2.Core.dll",
+        "webview/lib/Microsoft.Web.WebView2.WinForms.dll",
+        "webview/lib/WebBrowserInterop.x64.dll",
+        "webview/lib/WebBrowserInterop.x86.dll",
+    ]
+
+    for x in FILES:
+        path = f"{os.path.dirname(sys.argv[0])}/_internal/{x}:Zone.Identifier"
+        if os.path.exists(path):
+            os.unlink(path)
